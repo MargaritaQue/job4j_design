7d0ed4dbfedbadd3830291e2a387624cbedf6af8
margarita <margarita.que@mail.ru>
2025-07-04 01:37:42 +0400

    3. Формат XML [#313165]

---
+++ src/main/java/ru/job4j/serialization/xml/Main.java
@@ -0,0 +1,29 @@
+package ru.job4j.serialization.xml;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Cat cat = new Cat(false, 1, "Tolya",
+                new String[] {"Vaccinated", "Sterilized"});
+        JAXBContext context = JAXBContext.newInstance(Cat.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml;
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(cat, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Cat result = (Cat) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+
+    }
+}
