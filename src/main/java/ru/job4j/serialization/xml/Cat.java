dcdc7b6344449f9b58cad0154269bcaf0f7c413c
margarita <margarita.que@mail.ru>
2025-07-04 10:40:00 +0400

    4. JAXB. Преобразование XML в POJO. [#315063 #547552]

--- src/main/java/ru/job4j/serialization/xml/Cat.java
+++ src/main/java/ru/job4j/serialization/xml/Cat.java
@@ -1,12 +1,26 @@
 package ru.job4j.serialization.xml;
 
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.annotation.*;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "cat")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Cat {
-    private final boolean sex;
-    private final int age;
-    private final String name;
-    private final String[] statuses;
+
+    @XmlAttribute
+    private boolean sex;
+
+    @XmlAttribute
+    private int age;
+    private String name;
+
+    private String[] statuses;
+
+    public Cat() { }
 
     public Cat(boolean sex, int age, String name, String[] statuses) {
         this.sex = sex;
@@ -25,8 +39,20 @@ public class Cat {
                 + '}';
     }
 
-    public static void main(String[] args) {
-        final ru.job4j.serialization.json.Cat cat = new ru.job4j.serialization.json.Cat(false, 1, "Tolya",
+    public static void main(String[] args) throws JAXBException {
+        final Cat cat = new Cat(false, 1, "Tolya",
                 new String[] {"Vaccinated", "Sterilized"});
+
+        JAXBContext context = JAXBContext.newInstance(Cat.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(cat, writer);
+            String result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
