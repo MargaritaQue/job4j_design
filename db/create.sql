1c457cd622b90529771b4f112941d1116c79ddb2
margarita <margarita.que@mail.ru>
2025-07-18 20:56:05 +0400

    2. Схема прав: пользователи и роли. [#1093 #548195]

--- db/create.sql
+++ db/create.sql
@@ -1,36 +1,9 @@
-create table users (
-    id serial primary key,
-    name text
-    items_id int references items(id)
-);
-
-create table roles (
-    id serial primary key,
-    name text
-    users_id int references users(id)
-);
-
 create table rules (
     id serial primary key,
     name text
 );
 
-create table roles_rules (
-    id serial primary key,
-    roles_id int references roles(id),
-    rules_id int references rules(id)
-);
-
-create table items (
-    id serial primary key,
-    name text
-    comments_id int references comments(id)
-    attachs_id int references attachs(id)
-    categories_id int references categories(id)
-    states_id int references states(id)
-);
-
-create table comments (
+create table commentss (
     id serial primary key,
     name text
 );
@@ -40,13 +13,40 @@ create table attachs (
     name text
 );
 
+create table items (
+    id serial primary key,
+    name text,
+    commentss_id int references commentss(id),
+    attachs_id int references attachs(id)
+);
+
 create table states (
     id serial primary key,
-    name text
-
+    name text,
+	items_id int references items(id)
 );
 
 create table categories (
     id serial primary key,
-    name text
+    name text,
+	items_id int references items(id)
 );
+
+create table users (
+    id serial primary key,
+    name text,
+    items_id int references items(id)
+);
+
+create table roles (
+    id serial primary key,
+    name text,
+    users_id int references users(id)
+);
+
+create table roles_rules (
+    id serial primary key,
+    roles_id int references roles(id),
+    rules_id int references rules(id)
+);
+
